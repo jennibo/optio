4d573d7d7437fb8478da1f132d0d1357cf64375e
al1205an-s <al1205an-s@curie-18.net.lth.se>
2017-04-25 17:02:43 +0200

    implemented generaterays 1.2.3.1.0

--- intsec.m
+++ intsec.m
@@ -1,13 +1,17 @@
 function [ point ] = intsec( pf, dir, startX, startY)
-point = zeros(size(dir));
+point = zeros(size(dir))
+startY(1,1)
+startX(1,1)
+size(dir,2)
 
 for i=1:size(dir,2)
 %gives the position of the intersection
-g = @(x) (dir(2,i)./dir(1,i)).* x + startY - (dir(2,i).*startX)./dir(1,i);
+g = @(x) (dir(2,i)./dir(1,i)).* x + startY(1,1) - (dir(2,i).*startX(1,1))./dir(1,i);
 xpoint = fzero(@(x)g(x)-polyval(pf,x), 0);
 ypoint = g(xpoint);
 
 point(:,i) = [xpoint; ypoint];
+
 end
 end
 
