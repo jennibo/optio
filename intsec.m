3d673e81e326a2002ff51680f086531429670f05
al1205an-s <al1205an-s@curie-18.net.lth.se>
2017-04-25 16:00:15 +0200

    kinda finished everything

--- intsec.m
+++ intsec.m
@@ -1,6 +1,9 @@
-function [ point ] = intsec( f(x), dir, startX, startY)
+function [ point ] = intsec( pf, dir, startX, startY)
 %gives the position of the intersection
-g = @(x) (dir(1,1)./dir(2,1)).* x + start
+g = @(x) (dir(2,1)./dir(1,1)).* x + startY - (dir(2,1).*startX)./dir(1,1);
+xpoint = fzero(@(x)g(x)-polyval(pf,x), 0);
+ypoint = g(xpoint);
 
+point = [xpoint; ypoint];
 end
 
