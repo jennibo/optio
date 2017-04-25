019b017b7e8c6425f061ff85b0bffdee90e63abb
al1205an-s <al1205an-s@curie-18.net.lth.se>
2017-04-25 16:16:58 +0200

    added test 1.0.1.2

--- intsec.m
+++ intsec.m
@@ -1,9 +1,13 @@
 function [ point ] = intsec( pf, dir, startX, startY)
+point = zeros(size(dir));
+
+for i=1:size(dir,2)
 %gives the position of the intersection
-g = @(x) (dir(2,1)./dir(1,1)).* x + startY - (dir(2,1).*startX)./dir(1,1);
+g = @(x) (dir(2,i)./dir(1,i)).* x + startY - (dir(2,i).*startX)./dir(1,i);
 xpoint = fzero(@(x)g(x)-polyval(pf,x), 0);
 ypoint = g(xpoint);
 
-point = [xpoint; ypoint];
+point(:,i) = [xpoint; ypoint];
+end
 end
 
