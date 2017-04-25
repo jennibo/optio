5b955ec4c0bd4c305a01fcb4228f1183662c6343
patri <patri@DESKTOP-SC3772I>
2017-04-25 14:50:47 +0200

    refractRay function
    a unimplemented function that take lines, a position, refractive indexes and a polynomial function to produce new refracted lines at the given position.

---
+++ refractRay.m
@@ -0,0 +1,8 @@
+function [ rayStart, rayDir2 ] = refractRay( x, y, rayDir, p, n1, n2)
+%refractRay generates new refracted lines at a given position, original
+%lines, polynomial function and refraction indexes.
+%   Detailed explanation goes here
+
+
+
+end
