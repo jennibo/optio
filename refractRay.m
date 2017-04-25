e6740faf51685b6b671bd80003d027d948ab0efe
patri <patri@DESKTOP-SC3772I>
2017-04-25 16:06:24 +0200

    New updated refractRay

--- refractRay.m
+++ refractRay.m
@@ -1,8 +1,13 @@
-function [ rayStart, rayDir2 ] = refractRay( x, y, rayDir, p, n1, n2)
-%refractRay generates new refracted lines at a given position, original
-%lines, polynomial function and refraction indexes.
-%   Detailed explanation goes here
+function [ rayStart, rayDir2 ] = refractRay(hit, rayDir, p, n1, n2)
+%refractRay generates new refracted lines at a given position hit, original
+%lines rayDir, polynomial function, p and refraction indexes, n1 and n2.
 
+n = [-polyval(polyder(p), hit(:, 1)); ones(size(hit(:, 1)))]; %calculating the derivative of the function and using it to calculate the normal
+n = normc(n); 
+r = n1/n2;
+c = -repmat(dot(n, rayDir), 2, 1);
 
-
+rayDir2 = r*rayDir + (r*c - sqrt(1-r^2*(1-c.^2))).*n; %function can be found at https://en.wikipedia.org/wiki/Snell%27s_law
+rayDir2 = normc(rayDir2);
+rayStart = hit; %the orgin of the new refracted rays
 end
