4566fa85b357f875f7ac0d0627fb6bb7e20253a7
Johan Boström <johan.bostrom@mollehem.se>
2017-04-28 13:42:44 +0200

    Added return value refracted and argument inside in refractRay.m

--- refractRay.m
+++ refractRay.m
@@ -1,4 +1,4 @@
-function [ rayStart, rayDir2 ] = refractRay(hit, rayDir, p, n1, n2)
+function [ rayStart, rayDir2, refracted ] = refractRay(hit, rayDir, p, n1, n2, inside)
 %refractRay generates new refracted lines at a given position hit, original
 %lines rayDir, polynomial function, p and refraction indexes, n1 and n2.
 
@@ -7,20 +7,20 @@ n = [-polyval(polyder(p), hit(1, :)); ones(size(hit(1, :)))];
 n = normc(n); 
 
 %constants to the function below
-r = n1/n2;
+r = repmat((1 - inside)*n1/n2 + inside*n2/n1, 2, 1);
 c = -repmat(dot(n, rayDir), 2, 1);
 
 %the function that calculates the new refracted rays can be found at https://en.wikipedia.org/wiki/Snell%27s_law
 
 %refracted is a boolean matrix, with true for the refracted rays and false
 %for the reflected rays.
-refracted = 1 - r^2*(1 - c.^2) >= 0;
+refracted = 1 - r.^2.*(1 - c.^2) >= 0;
 
 %there are equal rays before and after the encounter with the surface
 rayDir2 = zeros(size(rayDir));
 
 %the direction of the refracted rays is calculated
-rayDir2(refracted) = r*rayDir(refracted) + (r*c(refracted) - sqrt(1-r^2*(1-c(refracted).^2))).*n(refracted);
+rayDir2(refracted) = r(refracted).*rayDir(refracted) + (r(refracted).*c(refracted) - sqrt(1-r(refracted).^2.*(1-c(refracted).^2))).*n(refracted);
 
 %the direction of the reflected rays is calculated
 rayDir2(not(refracted)) = reflectRay(rayDir(not(refracted)), n(not(refracted)), -c(not(refracted)));
